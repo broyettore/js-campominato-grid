663cf6c1dcd5a7eea5ef5960658193f50f22a0ee
broyettore <broyettore@gmail.com>
2023-03-02 20:32:52 +0100

    Added some comments

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,7 +4,7 @@
  * FUNZIONI
  *************/
 
-
+// function to create an element with a class
 function createElement(htmlElement, className) {
     const element = document.createElement(htmlElement);
     element.classList.add(className);
@@ -12,6 +12,7 @@ function createElement(htmlElement, className) {
     return element;
 }
 
+// function to select all elements of the same class and adding a click event
 function showColour(element) {
     const buttonSelect = document.querySelectorAll(element);
 
@@ -27,10 +28,10 @@ function showColour(element) {
  * MAIN
  *************/
 
-const ctnBoard = document.querySelector(".game-board");
-const selectMode = document.getElementById("difficulty");
-let selectModeValue = selectMode.value;
-const playBtn = document.querySelector(".play");
+const ctnBoard = document.querySelector(".game-board"); // selects container
+const selectMode = document.getElementById("difficulty"); // selects html select
+let selectModeValue = selectMode.value; // selects html select value
+const playBtn = document.querySelector(".play"); // selects play button
 let cellNumber = 100;
 
 for (let i = 1; i <= cellNumber; i++) {
@@ -38,7 +39,6 @@ for (let i = 1; i <= cellNumber; i++) {
     ctnBoard.append(createdElement);
     createdElement.innerText = i;
 }
-
 
 
 showColour(".game-cell");
