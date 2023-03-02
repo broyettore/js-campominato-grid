a2123c2860d6fc8d766feea360df33a69cde84dd
broyettore <broyettore@gmail.com>
2023-03-02 15:57:22 +0100

    Js exercise done, next is styling

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,38 @@
 'use strict';
 
+/*************
+ * FUNZIONI
+ *************/
 
+function createElement(htmlElement, className) {
+    const element = document.createElement(htmlElement);
+    element.classList.add(className);
+
+    return element;
+}
+
+function showColour(element) {
+    const buttonSelect = document.querySelectorAll(element);
+
+    buttonSelect.forEach(element => {
+        element.addEventListener("click", () => {
+                element.classList.toggle("blue");
+                console.log(element.innerText);
+            })
+        })
+    };
+
+/*************
+ * MAIN
+ *************/
+
+const ctnBoard = document.querySelector(".game-board");
+
+for (let i = 1; i <= 100; i++) {
+    const createdElement = createElement("div", "game-cell");
+    ctnBoard.append(createdElement);
+    createdElement.innerText = i;
+}
+
+showColour(".game-cell");
 
